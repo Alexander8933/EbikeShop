dc4c7d417c5bf317adccd3600047591f1786b356
Alexander8933 <polisx@gmail.com>
2023-03-18 17:38:07 +0200

    Change method update . class BikesCache.java

--- src/main/java/com/home/eshop/dao/BikesCache.java
+++ src/main/java/com/home/eshop/dao/BikesCache.java
@@ -25,19 +25,14 @@ public class BikesCache implements Dao {
         }
     }
 
-    private static void bikeNotFound() {
-        System.out.println("Bike not found");
-    }
-
     @Override
     public int update(Bike bike) {
         Optional<Bike> optionalBike = Optional.ofNullable(mapBikes.get(bike.getId()));
         optionalBike.ifPresent(bikeChang -> {
-            mapBikes.remove(bikeChang.getId());
             mapBikes.put(bikeChang.getId(), bikeChang);
         });
         optionalBike.orElse(bikesDao.findOne(bike.getId()));
-        optionalBike.ifPresentOrElse(bikeChang -> bikesDao.update(bikeChang), BikesCache::bikeNotFound);
+        optionalBike.ifPresent(bikeChang -> bikesDao.update(bikeChang));
         return bike.getId();
     }
 
@@ -48,7 +43,7 @@ public class BikesCache implements Dao {
             mapBikes.remove(bike.getId());
         });
         optionalBike.orElse(bikesDao.findOne(id));
-        optionalBike.ifPresentOrElse(bike -> bikesDao.delete(bike.getId()), BikesCache::bikeNotFound);
+        optionalBike.ifPresent(bike -> bikesDao.delete(bike.getId()));
     }
 
     @Override
@@ -65,12 +60,7 @@ public class BikesCache implements Dao {
     public Bike findOne(int id) {
         Optional<Bike> optionalBike = Optional.ofNullable(mapBikes.get(id));
         optionalBike.orElse(bikesDao.findOne(id));
-        if (optionalBike.isPresent()) {
-            return optionalBike.get();
-        } else {
-            System.out.println("Bike not found");
-        }
-        return null;
+        return optionalBike.orElse(null);
     }
 
     @Override
