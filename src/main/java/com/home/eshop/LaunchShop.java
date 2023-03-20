2e0c64f75fe2a8d90ec521f3d674d186382fb127
Alexander8933 <polisx@gmail.com>
2023-03-20 17:40:58 +0200

    test

--- src/main/java/com/home/eshop/LaunchShop.java
+++ src/main/java/com/home/eshop/LaunchShop.java
@@ -3,9 +3,8 @@ package com.home.eshop;
 import com.home.eshop.presentation.Menu;
 
 public class LaunchShop {
+
     public static void main(String[] args) {
         new Menu().show();
     }
-    //test
-    //test
 }
